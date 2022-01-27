c7f2307d687bc3e87e4704bfa1275f5d7c00f672
Joe Niemiec <joe.niemiec@gmail.com>
2022-01-27 07:26:36 -0600

    Bringing user state into the application via contextProvider

---
+++ context/user.js
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect, useContext } from 'react'
+import { supabase } from "../utils/supabase";
+
+const Context = createContext();
+
+const Provider = ({children}) => {
+    const [user, setUser] = useState(supabase.auth.user());
+
+    useEffect(() => {
+        supabase.auth.onAuthStateChange(() => {
+            setUser(supabase.auth.user())
+        })
+    }, []);
+
+    const exposed = {
+        user,
+    }
+
+    return <Context.Provider value={exposed}>{children}</Context.Provider>
+
+};
+
+export const useUser = () => useContext(Context);
+
+export default Provider;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import UserProvider from "../context/user";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>    
+  );
 }
 
 export default MyApp
