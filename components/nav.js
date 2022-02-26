10d2ccac0ed10f5f0375619a3fa77353e043e15e
Joe Niemiec <joe.niemiec@gmail.com>
2022-02-26 12:52:31 -0600

    Added ability for customer to view their subscription info if they are subscribed

--- components/nav.js
+++ components/nav.js
@@ -9,13 +9,21 @@ const Nav = () => {
         <nav className="flex py-4 px-6 border-b border-gray-200">
             <Link href='/'>
                 <a>Home</a>
-            </Link>     
+            </Link>
+            {!!user && (
+                <Link href="/dashboard">
+                    <a className="ml-2">Dashboard</a>
+                </Link>
+            )}    
             <Link href='/pricing'>
                 <a className="ml-2">Pricing</a>
             </Link>
-            <Link href={user ? "/logout" : "/login"}>
-                <a className="ml-auto">{user ? "Logout" : "Login"}</a>
-            </Link>
+            <div className="ml-auto">
+                <Link href={user ? "/logout" : "/login"}>
+                    <a>{user ? "Logout" : "Login"}</a>
+                </Link>
+            </div>
+            
         </nav>
     )
 };
